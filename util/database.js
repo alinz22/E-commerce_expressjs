11da8747c19eae59587e5e30e9bc5da06c25ef8c
Alan Mong <alanmongx@gmail.com>
2024-08-05 02:31:00 -0700

    add sql database and functions

---
+++ util/database.js
@@ -0,0 +1,12 @@
+// util/database.js
+
+const mysql = require("mysql2");
+
+const pool = mysql.createPool({
+  host: "localhost",
+  user: "root",
+  database: "node-complete",
+  password: "Butter",
+});
+
+module.exports = pool.promise();
