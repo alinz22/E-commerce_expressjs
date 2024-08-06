1d2525a5f82d0825ad85166f59e19da6731f99a2
Alan Mong <alanmongx@gmail.com>
2024-08-05 23:10:00 -0700

    connect to mongodb

--- util/database.js
+++ util/database.js
@@ -1,8 +1,24 @@
-const Sequelize = require("sequelize");
+// const Sequelize = require("sequelize");
 
-const sequelize = new Sequelize("node-complete", "root", "Butter", {
-  dialect: "mysql",
-  host: "localhost",
-});
+// const sequelize = new Sequelize("node-complete", "root", "Butter", {
+//   dialect: "mysql",
+//   host: "localhost",
+// });
 
-module.exports = sequelize;
+// module.exports = sequelize;
+
+const mongodb = require("mongodb");
+const MongoClient = mongodb.MongoClient;
+
+const mongoConnect = (callback) => {
+  MongoClient.connect(
+    "mongodb+srv://butter:food@cluster0.x7ufutg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+  )
+    .then((client) => {
+      console.log("Connected to MongoDB!");
+      callback(client);
+    })
+    .catch((err) => console.log(err));
+};
+
+module.exports = mongoConnect;
