5ad8a26902b841f08d73bf7cf0064ca2af2ac53f
Alan Mong <alanmongx@gmail.com>
2024-08-06 21:37:40 -0700

    session cookies

--- routes/auth.js
+++ routes/auth.js
@@ -1,11 +1,11 @@
 const express = require("express");
 
-const authControllers = require("../controllers/auth");
+const authController = require("../controllers/auth");
 
 const router = express.Router();
 
-router.get("/login", authControllers.getLogin);
+router.get("/login", authController.getLogin);
 
-router.post("/login", authControllers.postLogin);
+router.post("/login", authController.postLogin);
 
 module.exports = router;
