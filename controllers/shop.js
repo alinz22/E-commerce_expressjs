b07b78914b93e82af5b568608f21cfd4411ddbf1
Alan Mong <alanmongx@gmail.com>
2024-08-09 12:18:39 -0700

    add paginatino

--- controllers/shop.js
+++ controllers/shop.js
@@ -4,6 +4,8 @@ const Order = require("../models/order");
 const fs = require("fs");
 const path = require("path");
 
+const ITEMS_PER_PAGE = 2;
+
 exports.getProducts = (req, res, next) => {
   Product.find()
     .then((products) => {
@@ -33,7 +35,11 @@ exports.getProduct = (req, res, next) => {
 };
 
 exports.getIndex = (req, res, next) => {
+  const page = req.query.page;
+
   Product.find()
+    .skip((page - 1) * ITEMS_PER_PAGE)
+    .limit(ITEMS_PER_PAGE)
     .then((products) => {
       res.render("shop/index", {
         prods: products,
